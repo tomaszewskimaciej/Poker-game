60fcb66d31b061afcbce15011e848158d669cb4a
Maciek <tomaszewski.maciej67@gmail.com>
2022-05-31 18:07:17 +0200

    next

--- src/main/Check.java
+++ src/main/Check.java
@@ -7,13 +7,13 @@ public class Check {
         if (hands.get(0).getRank() > 8 && hands.get(2).getRank() > 8) {
             return true;
         }
-        if (hands.get(0).getRank() == 0 && hands.get(2).getRank() == 0) {
+        if (hands.get(0).getRank() == 12 && hands.get(2).getRank() == 12) {
             return true;
         }
-        if (hands.get(0).getRank() == 0 && hands.get(2).getRank() > 8) {
+        if (hands.get(0).getRank() == 12 && hands.get(2).getRank() > 8) {
             return true;
         }
-        if (hands.get(0).getRank() > 8 && hands.get(2).getRank() == 0) {
+        if (hands.get(0).getRank() > 8 && hands.get(2).getRank() == 12) {
             return true;
         }
         if (hands.get(0).getRank() == hands.get(2).getRank()) {
@@ -23,14 +23,6 @@ public class Check {
             return true;
         }
         return false;
-    }
-
-    static boolean checkBoard(ArrayList<Card> board) {
-        ArrayList<Card> aaa = new ArrayList<>();
-        for (int i = 0; i < board.size(); i++) {
-            aaa.add(board.get(i));
-        }
-        return true;
     }
 
     static int finalCheck(ArrayList<Card> hands, ArrayList<Card> board, int playerOrBot) {
@@ -71,69 +63,109 @@ public class Check {
     int fourOfKind(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
         if (checkIt.size() != checkUnique.size()) {
             return 1;
-        } else {
-            return 0;
         }
-
+        return 0;
     }
 
     int fullHouse(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
+        boolean pair = false;
+        boolean three = false;
         if (checkIt.size() != checkUnique.size()) {
-            return 1;
-        } else {
-            return 0;
+            for (int a = 0; a < checkIt.size() - 2; a++) {
+                if (checkIt.get(a).getRank() == checkIt.get(a + 1).getRank()) {
+                    if (checkIt.get(a).getRank() == checkIt.get(a + 2).getRank()) {
+                        three = true;
+                        a += 2;
+                    } else {
+                        pair = true;
+                        a += 1;
+                    }
+                }
+            }
         }
-
+        if (pair && three) {
+            return 7;
+        }
+        return 0;
     }
 
     int flush(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
-        return 1;
-
+        int diamonds = 0;
+        int clubs = 0;
+        int hearths = 0;
+        int spades = 0;
+        for (int a = 0; a < checkIt.size(); a++) {
+            if (checkIt.get(a).getSuit() == 0) {
+                clubs++;
+            }
+            if (checkIt.get(a).getSuit() == 1) {
+                diamonds++;
+            }
+            if (checkIt.get(a).getSuit() == 2) {
+                hearths++;
+            }
+            if (checkIt.get(a).getSuit() == 3) {
+                spades++;
+            }
+        }
+        if (clubs >= 5 || diamonds >= 5 || hearths >= 5 || spades >= 5) {
+            return 6;
+        }
+        return 0;
     }
 
     int straight(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
-        return 1;
-
+        int straightLength = 0;
+        for (int a = 0; a < checkIt.size(); a++) {
+            if (checkIt.get(a).getRank() == checkIt.get(a + 1).getRank() - 1) {
+                straightLength++;
+            } else {
+                straightLength = 0;
+            }
+        }
+        if (straightLength >= 5) {
+            return 5;
+        }
+        return 0;
     }
 
     int threeOfKind(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
-        if (checkIt.size()-2 == checkUnique.size()) {
-            return 1;
-        } else {
-            return 0;
+        if (checkIt.size() - 2 == checkUnique.size()) {
+            for (int a = 1; a < checkIt.size() - 1; a++) {
+                if (checkIt.get(a).getRank() == checkIt.get(a - 1).getRank() && checkIt.get(a).getRank() == checkIt.get(a + 1).getRank()) {
+                    return 4;
+                }
+            }
         }
-
+        return 0;
     }
 
     int twoPair(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
         //I'm aware that this method might mistake 3 same cards as a 2 pairs but it does not matter as it means that there's Three of a kind which is stronger
         int howManyPairs = 0;
-        if (checkIt.size()-2 == checkUnique.size()) {
-            for (int a = 0; a < checkIt.size() - 1; a++) {
-                for (int b = a+1; b < checkIt.size(); b++) {
-                    if (checkIt.get(a).getRank() == checkIt.get(b).getRank()) {
-                        howManyPairs+=1;
-                        break;
-                    }
-                }
+        for (int a = 0; a < checkIt.size() - 1; a++) {
+            if (checkIt.get(a).getRank() == checkIt.get(a + 1).getRank()) {
+                howManyPairs += 1;
+                a += 1;
             }
         }
-        if(howManyPairs==2){
+        if (howManyPairs == 2) {
             return 3;
         }
         return 0;
     }
 
-    int twoPairOrThreeOfAKind(ArrayList<Card> checkIt, TreeSet<Card> checkUnique){
-        if(checkIt.size()-1==checkUnique.size()){
-            for(int a = 0; a < checkIt.size()-1;a++){
+    int twoPairOrThreeOfAKind(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
+        if (checkIt.size() - 1 == checkUnique.size()) {
+            for (int a = 0; a < checkIt.size() - 1; a++) {
 
             }
         }
         return 0;
     }
+
     int pair(ArrayList<Card> checkIt, TreeSet<Card> checkUnique) {
-        if(checkIt.size()==checkUnique.size()-1){
+        if (checkIt.size() == checkUnique.size() - 1) {
             return 2;
         }
         return 0;
