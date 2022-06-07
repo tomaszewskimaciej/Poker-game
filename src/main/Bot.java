735d5fdf98ad216aea5762dfd9e2fa38e31fab77
Tomaszewski Maciej <tomaszewski.maciej67@gmail.com>
2022-06-07 14:13:34 +0200

    Finished.

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -34,15 +34,18 @@ public class Bot {
     }
 
     /*
-    About bots level, there are 3 different bots level, where 1 is the easiest one:
-        Level 1: In this difficulty you are allowed to see bot's cards. Will never pass, will never outbid player.
-        Level 2: Will never pass, will never outbid player.
-        Level 3: Might pass if player bids too much, analyses starting hand but he may check even though he doesn't consider his hand as good. To check what is good hand look at bot level 4. He is not able to compare his cards with board cards.
-        Level 4: Highest level possible atm. Analyses starting hand, hands he considers as good are pairs; 2 high value cards, where he considers 10 as the first high value card; 2 cards of the same suit. Compares his cards to board cards.
-        TODO Level 4: This one could be able to check its own cards and player's card and then calculate its chance to win, using math algorithm.
+    About bots level, there are 4 different bots level, where 1 is the easiest one:
+        Level 1: You are allowed to see bot’s cards. It’ll never pass or outbid the player.
+        Level 2: It’ll never pass or outbid the player.
+        Level 3: He’s able to analyze the starting hand (only the first 2 cards), you can read about the starting hand it considers as good below. It might outbid the player if his starting hand was good and might pass if the player bids too much.
+        Level 4: It’s able to compare its hand with board cards and search for combinations of cards. It might play aggressively (e.g. outbid by huge amounts of money) if it has a good combination. If the player bids too much it’ll check all the combinations before making a decision. It’s able to analyze the starting hand like a level 3 bot.
 
-        Outbiding after 2 first card is not allowed, bots never pass after first 2cards.
-        Game is over when either of players has no money left or when either of players have no money for bighand.
+        The starting hands that bots level 3 and 4 consider as ‘good’ are:
+        pair, 2 high value cards where 10 is the first high value cards, 2 cards of the same suit.
+        TODO Level 5: This one could be able to check its own cards and player's card and then calculate its chance to win, using math algorithm.
+
+        Outbidding after 2 first card is not allowed.
+        Game is over when either of players has no money left or when either of players have no money for big hand.
 
      */
 }
